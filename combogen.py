90c2de4832677ad9051629dba4392aed76b8f649
Dark Hunter 141 <78160549+darkhunter141@users.noreply.github.com>
2021-08-16 19:09:44 +0600

    Create combogen.py

---
+++ combogen.py
@@ -0,0 +1,11 @@
+#coded by DarkXploit
+import os, random
+gppass = "017"
+gp = "88017"
+for i in range (10) :
+  randomn = random.randint(11111111,99999999)
+  rannum = str (randomn)
+  number = gp + rannum
+  password = gppass + rannum
+  open ("combo.txt" ,"a").write("\n"+number + ":" + password )
+  
